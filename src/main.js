6825cc094130f9fbce4c70fa5cdf7a5dd15828aa
BarbaraSouza <babiholiveira@outlook.com>
2021-06-13 17:15:36 -0300

    Add Vuex

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 'use strict';
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import App from './App.vue';
 import VueRouter from 'vue-router';
 import {router} from './router.js';
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
 Vue.config.productionTip = false;
 Vue.use(VueRouter);
 Vue.use(Vuetify);
+Vue.use(Vuex);
 new Vue({
   router,
   vuetify : new Vuetify(),
