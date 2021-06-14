2ece396a1daef4df4f8c8ad33713680a0e88f8a3
BarbaraSouza <babiholiveira@outlook.com>
2021-06-14 09:54:17 -0300

    add NotFound Page

---
+++ src/Views/NotFound.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+    <Navbar name=" "/>
+    <div class="container-notFound">
+        <h3>Ops, pagina nao encontrada!</h3>
+        <router-link 
+                to="/"
+                style="color: #FF9D00"
+        >Voltar para Home.</router-link>
+    </div>
+</div>
+</template>
+<script>
+import Navbar from "../components/Navbar.vue";
+export default {
+    components: {
+        Navbar
+    },
+}
+</script>
+
+<style>
+.container-notFound {
+  margin: 2rem;
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  justify-items: center;  
+  flex-direction: column;
+  border-radius: 10px 10px 10px 10px;
+  border: 1px solid rgb(187, 187, 187);
+  box-shadow: -1px 8px 14px -9px rgba(92,92,92,0.75);
+  text-align: center;
+}
+.container-notFound h3 {
+  font-size: 15px;
+  font-family: 'Oswald', sans-serif;
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -3,10 +3,12 @@ import VueRouter from 'vue-router';
 import Home from "./Views/Home.vue";
 import Create from "./Views/Create.vue";
 import List from "./Views/List.vue";
+import NotFound from "./Views/NotFound.vue"
 const routes = [
     {path: '/', name: 'Home', component: Home},
     {path: '/criar',name: 'Create', component: Create, props: true},
-    {path: '/listar',name: 'List', component: List, props: true}
+    {path: '/listar',name: 'List', component: List, props: true},
+    {path: '*', component: NotFound},
 ]
 
 export const router = new VueRouter({
